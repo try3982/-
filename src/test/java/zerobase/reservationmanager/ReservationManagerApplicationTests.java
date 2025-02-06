b9f2b763d2458ec5d826371b0866a826cf03b2ba
WonJin <try3982@kakao.com>
2025-02-06 23:14:59 +0900

    배당금 정보 불러오기

---
+++ src/test/java/zerobase/reservationmanager/ReservationManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package zerobase.reservationmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
